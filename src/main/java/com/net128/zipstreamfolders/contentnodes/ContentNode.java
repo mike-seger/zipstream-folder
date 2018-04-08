80aee3d2beaa88631816dc8eb2e9d05e219748fd
mike-seger <mss@unknown.com>
2018-04-08 05:59:08 +0200

    cleaned up more

--- src/main/java/com/net128/zipstreamfolders/contentnodes/ContentNode.java
+++ src/main/java/com/net128/zipstreamfolders/contentnodes/ContentNode.java
@@ -99,7 +99,7 @@ public class ContentNode implements Iterable<ContentNode> {
 	@Override
 	public Iterator<ContentNode> iterator() {
 		List<Iterable<ContentNode>> iterables=new ArrayList<>();
-		if(!isStucture() && getPath()!=null) {
+		if(!isStucture() && getPath()!=null && hasData()) {
 			iterables.add(Collections.singletonList(this));
 		}
 		if(children!=null) {
